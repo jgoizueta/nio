f8a51e43ff836ab523e99b94c4ddc97ba72bd3ac
jgoizueta <jgoizueta@e0d2ef34-2163-4af7-bbef-dc9412234f94>
2007-12-13 17:10:34 +0000

    modification of example printer show.rb to not require predefinition of variables
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nio/trunk@30 e0d2ef34-2163-4af7-bbef-dc9412234f94

--- source/helpers/show.rb
+++ source/helpers/show.rb
@@ -1,11 +1,9 @@
+require 'rubygems'
 input = File.read('show_nio.rb')
 File.open('show.txt','a') do |file|
   
   #$stdout = file
   out = StringIO.new
-
-  # vars must be predefined
-  x = y = z = fmt = nil
   
   indent = 4
   align = 60
@@ -13,7 +11,7 @@ File.open('show.txt','a') do |file|
   input.each do |line|
     file.write line.chomp
     $stdout = StringIO.new
-    eval line
+    eval line, TOPLEVEL_BINDING
     out = $stdout.string
     unless out.empty?
       file.write " "*[0,(align-line.size)].max + " -> " + out.chomp

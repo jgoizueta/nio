92b4e825ff3f9bc1263ef2a069596f9cce940ab3
jgoizueta <jgoizueta@e0d2ef34-2163-4af7-bbef-dc9412234f94>
2008-03-04 16:24:41 +0000

    Correction of revisions 34 & 35 done due to a pathetic mental lapse: I forgot this project's source was ruweb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/nio/trunk@38 e0d2ef34-2163-4af7-bbef-dc9412234f94

--- lib/nio.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift File.dirname(__FILE__)
-
-require 'nio/version'
-require 'nio/tools'
-require 'nio/flttol'
-require 'nio/repdec'
-require 'nio/rtnlzr'
-require 'nio/fmt'
--- lib/nio/version.rb
+++
@@ -1,9 +0,0 @@
-module Nio #:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 2
-    TINY  = 1
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
